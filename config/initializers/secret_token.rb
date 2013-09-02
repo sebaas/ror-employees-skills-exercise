e70b6804b2d4a65c916cc24df182a7cffa0857a6
Sebastian Scandolo <seba.as@gmail.com>
2013-09-02 12:44:08 -0300

    First commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmployeesSkill::Application.config.secret_key_base = 'c268d5aa2fc7ac4e4763cfe1a078cc4ed1db9010dafad5bbfc70e3ab69185bad4988b8b2037ec3366d84d1a89503a5a001a7ec2f7d93e3e303ade12b54bcb9aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EmployeesSkill::Application.config.session_store :cookie_store, key: '_EmployeesSkill_session'
